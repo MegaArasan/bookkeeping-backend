e2590d788c60191b470a50a4601fde9a8ef516f7
MegaArasan <megaarasan2000@gmail.com>
2022-02-01 17:13:40 +0530

    with pdf

--- routes/invoices.js
+++ routes/invoices.js
@@ -2,6 +2,8 @@ import express from "express";
 const router = express.Router();
 import { client } from "../index.js";
 import { ObjectId } from "mongodb";
+import pdf from "html-pdf";
+import pdfTemplate from "../documents/index.js";
 
 router.route("/createinvoice").post(async (req, res) => {
   const data = req.body;
@@ -61,4 +63,56 @@ router.route("/delete/:id").delete(async (req, res) => {
   res.status(200).send(invoices);
 });
 
+router.route("/createanddownloadpdf").post((req, res) => {
+  pdf.create(pdfTemplate(req.data), {}).toFile("Invoice.pdf", (err) => {
+    if (err) {
+      return promise.reject();
+    }
+    return promise.resolve();
+  });
+});
+router.route("/fetch-pdf").get((req, res) => {
+  res.sendFile(`${__dirname}/Invoice.pdf`);
+});
+
+router.route("/send-pdf").post((req, res) => {
+  const data = req.body;
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: `${process.env.my_gmail}`,
+      pass: `${process.env.my_pass}`,
+    },
+  });
+
+  const mailoptions = {
+    from: data.company.businessName,
+    to: data.email,
+    subject: "Receipt of bill paym,ent",
+    html: `
+  <header><img src=${data.company.logo} alt="logo" style={{width:"100px"}} /></header>
+  <h1>Hello ${data.name}</h1>
+    <p>This mail for just remember you for the payment of bill.</p> 
+    <p>your due amount is ${data.balanceDue}</p>
+    <p>please pay the bill before or on deu date.</p>
+    <p>Your due date is ${data.dueDate}</p>
+  `,
+    attachments: [
+      {
+        filename: "Invoice.pdf",
+        path: __dirname + "Invoice.pdf",
+        contentType: "application/pdf",
+      },
+    ],
+  };
+
+  transporter.sendMail(mailoptions, (err, res) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Email sent: " + res.response);
+    }
+  });
+  res.status(200).send({ Msg: "recovery mail sent" });
+});
 export const invoiceRouter = router;
